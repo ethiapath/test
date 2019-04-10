8dc30f6216f66c832d25290384c703ee784c0c02
Hargobind Atwal <ethaipath@gmail.com>
2019-04-09 18:07:17 -0700

    enums. what are they

---
+++ c/enum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+enum boolean { false, true };
+enum boolean check;
+
+
+
+
+int main(void) {
+
+  //enum boolean;
+
+  printf("enum boolean: %s \n", check);
+
+
+  return 0;
+}
